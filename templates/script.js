48858241a5fbc093f33299f05c1a42a68a0e4d2d
harsh1x4 <harshbsi567@gmail.com>
2022-12-02 13:19:01 +0530

    uploading file preview added

--- templates/script.js
+++ templates/script.js
@@ -4,3 +4,20 @@ fileInput = document.querySelector(".file-input")
 inputWrapper.addEventListener("click", () => {
   fileInput.click();
 });
+
+fileInput.onchange = ({target}) => {
+  let files = target.files;
+  console.log(files.length)
+
+  if (files.length <= 2) {
+    for (let index = 0; index < files.length; index++) {
+      console.log(files[index].name)
+      document.getElementById('uploaded').innerHTML += `<div class="uploaded_files" id="uploaded_files" ><img src="./doc.svg" alt="document svg"> <span id="filename">${files[index].name}</span></div>`
+    }
+  }else{
+    document.getElementById('uploaded').innerHTML = `<div class="uploaded_files" id="uploaded_files" ><img src="./error.svg" alt="document svg"> <span id="filename">Error!!! Refresh the page and try uploading two files.</span></div>`
+    document.getElementById('filename').style.color = "#ff0000"
+    document.getElementById('uploaded_files').style.borderColor = "#ff0000"
+    document.getElementById('filename').style.fontWeight = "600"
+  }
+}
